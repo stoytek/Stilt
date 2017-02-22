485e6878cc18663eaa1fbdbfb40ca5ab39a04fbc
Frode Jacobsen <frodejac@users.noreply.github.com>
2017-02-22 14:45:24 +0100

    Added a test for supported sample rates

--- app/src/main/java/com/stilt/stoytek/stilt/MainActivity.java
+++ app/src/main/java/com/stilt/stoytek/stilt/MainActivity.java
@@ -3,12 +3,14 @@ package com.stilt.stoytek.stilt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.media.AudioRecord;
+import android.media.AudioFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("audio-recorde-jni");
+        System.loadLibrary("audio-recorder-jni");
     }
 
     @Override
@@ -20,4 +22,35 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Hello world!");
     }
+
+
+
+    /* Check for valid sample rates, pass highest possible samplerate to createAudioEngine */
+    /* TODO: This might be possible to do with OpenSLES
+     *  SLAudioIODeviceCapabilitiesItf_->QuerySampleFormatsSupported(SLAudioIODeviceCapabilitiesItf self,
+	 *	SLuint32 deviceId,
+	 *	SLmilliHertz samplingRate,
+	 *	SLint32 *pSampleFormats,
+	 *	SLint32 *pNumOfSampleFormats);
+     */
+
+    private int getMaxSupportedSampleRate() {
+
+        final int validSampleRates[] = new int[] { 8000, 11025, 16000, 22050,
+                32000, 37800, 44056, 44100};
+
+        for (int i = 0; i < validSampleRates.length; i++) {
+            int result = AudioRecord.getMinBufferSize(validSampleRates[i],
+                    AudioFormat.CHANNEL_IN_DEFAULT,
+                    AudioFormat.ENCODING_DEFAULT);
+            if (result != AudioRecord.ERROR
+                    && result != AudioRecord.ERROR_BAD_VALUE && result > 0) {
+                return validSampleRates[i];
+            }
+        }
+
+        return -1;
+    }
+
+
 }
