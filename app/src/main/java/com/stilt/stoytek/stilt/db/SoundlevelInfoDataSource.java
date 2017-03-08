e6a3cb87b6934b90834479977bd52983f940bc9f
Frode Jacobsen <frodejac@users.noreply.github.com>
2017-03-08 15:41:16 +0100

    Started work on the funfact database.

--- app/src/main/java/com/stilt/stoytek/stilt/db/SoundlevelInfoDataSource.java
+++ app/src/main/java/com/stilt/stoytek/stilt/db/SoundlevelInfoDataSource.java
@@ -1,8 +1,27 @@
 package com.stilt.stoytek.stilt.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by frodeja on 08/03/17.
  */
 
-public class SoubdlevelInfoDataSource {
+public class SoundlevelInfoDataSource {
+
+    private SQLiteDatabase db;
+    private SoundlevelInfoSQLiteHelper dbHelper;
+
+    private static final String[] allColumns = {
+            SoundlevelInfoSQLiteHelper.COLUMN_ID,
+            SoundlevelInfoSQLiteHelper.COLUMN_TEXT,
+            SoundlevelInfoSQLiteHelper.COLUMN_SOUNDLEVEL
+    };
+
+    public SoundlevelInfoDataSource(Context context) {
+        /* TODO: Add a static method in SoundlevelInfoSQLiteHelper to check if DB exists */
+        dbHelper = new SoundlevelInfoSQLiteHelper(context);
+    }
+
+
 }
--- app/src/main/java/com/stilt/stoytek/stilt/db/SoundlevelInfoSQLiteHelper.java
+++ app/src/main/java/com/stilt/stoytek/stilt/db/SoundlevelInfoSQLiteHelper.java
@@ -1,9 +1,35 @@
 package com.stilt.stoytek.stilt.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by frodeja on 08/03/17.
  */
 
-public class SoundlevelInfoSQLiteHelper {
+public class SoundlevelInfoSQLiteHelper extends SQLiteOpenHelper {
+    public static final String TABLE_SLINFO = "slinfo";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TEXT = "text";
+    public static final String COLUMN_SOUNDLEVEL = "soundlevel";
 
+    private static final String DATABASE_NAME = "slinfo_db";
+    private static final int DATABASE_VERSION = 1;
+
+    public SoundlevelInfoSQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        /* TODO: Copy the database from the assets folder */
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        /* TODO: Delete the database file */
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SLINFO);
+        onCreate(db);
+    }
 }
