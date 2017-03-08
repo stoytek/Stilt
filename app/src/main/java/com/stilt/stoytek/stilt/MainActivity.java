0223c3f1b37959a1cd90bedb909d468cede4e285
martidor <martin2609@hotmail.com>
2017-03-08 11:18:26 +0100

    fixed more merge conflicts

--- app/src/main/java/com/stilt/stoytek/stilt/MainActivity.java
+++ app/src/main/java/com/stilt/stoytek/stilt/MainActivity.java
@@ -4,15 +4,6 @@ package com.stilt.stoytek.stilt;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.media.AudioRecord;
-import android.media.AudioFormat;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.view.MotionEvent;
-import android.widget.TextView;
 import com.stilt.stoytek.stilt.audiorec.AudioCallback;
     
 
@@ -44,13 +35,13 @@ public class MainActivity extends FragmentActivity implements AudioCallback {
 
     @Override
     public void notifyAudioReady() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                TextView tv = (TextView) findViewById(R.id.sample_text);
-                tv.setText("Audio is ready");
-                findViewById(R.id.record).setBackgroundColor(Color.GREEN);
-            }
-        });
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                TextView tv = (TextView) findViewById(R.id.sample_text);
+//                tv.setText("Audio is ready");
+//                findViewById(R.id.record).setBackgroundColor(Color.GREEN);
+//            }
+//        });
     }
 }
